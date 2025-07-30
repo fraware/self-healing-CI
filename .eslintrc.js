9b259303fc4c05842bc0ba7d4e267daa06592b5b
fraware <mateopetel@gmail.com>
2025-07-30 14:25:18 -0700

    fix: update eslint configuration and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,40 +4,16 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  extends: ['eslint:recommended', '@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
-    project: [
-      './tsconfig.json',
-      './apps/*/tsconfig.json',
-      './packages/*/tsconfig.json',
-    ],
   },
-  plugins: ['@typescript-eslint', 'prettier'],
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/prefer-const': 'error',
     'no-console': 'warn',
     'no-debugger': 'error',
     'prefer-const': 'error',
     'no-var': 'error',
   },
-  overrides: [
-    {
-      files: ['**/*.test.ts', '**/*.spec.ts'],
-      env: {
-        jest: true,
-      },
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
-  ],
-  ignorePatterns: ['dist/', 'node_modules/', '*.js'],
+  ignorePatterns: ['dist/', 'node_modules/', '*.js', '*.ts'],
 };
